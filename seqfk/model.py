7895358e7d19cd7c1533125f0afffd9c483bd5df
Lennard Kwakernaak <lennardkwakernaak@gmail.com>
2020-07-16 15:37:26 +0200

    Cleanup

--- seqfk/model.py
+++ seqfk/model.py
@@ -1,7 +1,7 @@
 import numba as nb
 import numpy as np
 
-from parameters import equilibrium, stiffness, ALPHA, LENGTH, R_EFF, GAMMA, LD, RLD, reverse_pair_lookup
+from parameters import equilibrium, stiffness, ALPHA, LENGTH, R_EFF, GAMMA, LD, RLD
 
 
 @nb.njit()
@@ -124,9 +124,6 @@ def rise_potential(floating_index, sequence):
         potential[i] = 1 / 2 * springconstant * stretch ** 2
     return potential
 
-
-# shift slide   rise    tilt    roll    twist
-# 0     1       2       3       4       5
 
 @nb.njit()
 def roll_potential(floating_index, sequence):
@@ -267,153 +264,3 @@ def tension_energy(floating_index, deformation, force_constant=average_rise_stif
     dE_2 = force_constant * dx_2 ** 2
 
     return dE_1 + dE_2
-
-    # at the one end
-    # dx_1 = floating_index[0] + d # stretched by amount d
-    # dx_1 = (floating_index[0]-0) + d
-    # E_1 = 1/2 * springconstant * dx_1**2
-
-    # at the other end
-    # dx_2 = floating_index[-1] - len(floating_index)+1 + d
-    # dx_2 = (d + len(floating_index)) - floating_index[-1]
-    # dx_2 = len(floating_index)-1 + d - floating_index[-1]
-    # end_root = len(floating_index)-1
-    # dx_2 = (floating_index[end_root] - end_root) - d
-    # E_2 = 1/2 * springconstant * dx_2**2
-
-    # return E_1 + E_2
-
-
-@nb.njit()
-def pulling_energy(floating_index, deformation, index=0, springconstant=average_rise_stiff):
-    """
-    Similar to tension energy but for only one pulling direction. Here deformaton is the complete deformation at index
-    :param floating_index: The floating point indices of the dna
-    :param deformation: The distance in floating point "units" to pull the DNA
-    :param index: At what actual DNA index to pull
-    :param springconstant: The springconstant used for the deformation.
-    :return:
-    """
-    d = deformation  # stretch the string on both sides with half of the deformation
-    # at the one end
-    dx_1 = floating_index[index] - index - d  # stretched by amount d
-    E_1 = 1 / 2 * springconstant * dx_1 ** 2
-
-    E_1
-
-
-if __name__ == '__main__':
-    def create_table():
-        import pandas as pd
-
-        dinucleotide = [reverse_pair_lookup[i] for i in range(16)]
-        d_1 = [i[0] for i in dinucleotide]
-        d_2 = [i[1] for i in dinucleotide]
-
-        K_tilt = [stiffness[RLD[d_1[i]], RLD[d_2[i]], 3, 3] for i in range(len(dinucleotide))]
-        theta_tilt = [equilibrium[RLD[d_1[i]], RLD[d_2[i]], 3] for i in range(len(dinucleotide))]
-
-        K_roll = [stiffness[RLD[d_1[i]], RLD[d_2[i]], 4, 4] for i in range(len(dinucleotide))]
-        theta_roll = [equilibrium[RLD[d_1[i]], RLD[d_2[i]], 4] for i in range(len(dinucleotide))]
-
-        K_rise = [stiffness[RLD[d_1[i]], RLD[d_2[i]], 2, 2] for i in range(len(dinucleotide))]
-        theta_rise = [equilibrium[RLD[d_1[i]], RLD[d_2[i]], 2] for i in range(len(dinucleotide))]
-
-        df = pd.DataFrame([dinucleotide, theta_tilt, K_tilt, theta_roll, K_roll, theta_rise, K_rise]).transpose()
-        df.columns = ['Identity', '$q^{tilt, eq}$', '$K^{tilt, eq}$', '$q^{roll, eq}$', '$K^{roll, eq}$',
-                      '$q^{rise, eq}$', '$K_{rise, eq}$']
-        # df['theta_tilt_deg'] = df['theta_tilt'] * 180 / np.pi
-        # df['theta_roll_deg'] = df['theta_roll'] * 180 / np.pi
-        # df['K_tilt_deg'] = df['K_tilt'] * (np.pi / 180) ** 2
-        # df['K_roll_deg'] = df['K_roll'] * (np.pi / 180) ** 2
-
-        return df
-#
-#     def diff(A, B):
-#         for i, letter in enumerate(A):
-#             if B[i] != letter:
-#                 return False
-#         return True
-#
-#
-#     s = "ACGTCGTACGTCGGGTATAAAT"
-#     print(s)
-#     encoded_s = encode(s)
-#     print("encoded_s", encoded_s)
-#     decoded_s = decode(encoded_s)
-#     print("decoded_s", decoded_s)
-#     print("Matches", diff(s, decoded_s))
-#
-#     from sequence import Sequence
-#
-#     s = Sequence()
-#     # print(calc_chain_force(s.positions, s.sequence))
-#
-#     # print(equilibrium_chain_positions(s.sequence))
-#     print(zuiddam_positions(s.sequence))
-#
-#     # rot = rotation_along_superhelix(s.sequence)
-#
-#     df = create_table()
-#     import pandas as pd
-#
-#     pd.set_option('display.max_rows', 20, 'display.max_columns', 20)
-#
-#     print(df)
-#
-#     import matplotlib.pyplot as plt
-#
-#     # for i in range(4):
-#     #     p = tilt_potential(s.positions, np.ones_like(s.sequence)*i)
-#     #     p2 = roll_potential(s.positions, np.ones_like(s.sequence) * i)
-#     #     p3 = averaged_tilt_potential(s.positions)
-#     #     p4 = averaged_roll_potential(s.positions)
-#     #     plt.plot(p, label=str(i)+'tilt')
-#     #     plt.plot(p2, label=str(i) + 'roll')
-#     #     plt.plot(p3, label=str(i) + 'average_tilt')
-#     #     plt.plot(p4, label=str(i) + 'averate_roll')
-#     #     plt.legend()
-#     #     plt.show(block=True)
-#
-#     positions = zuiddam_floating_index(s.positions)
-#
-#     for i in range(4):
-#         p = tilt_potential(positions, np.ones(len(positions), dtype=np.uint8) * i)
-#         # p2 = roll_potential(s.positions, np.ones_like(s.sequence) * i)
-#         p3 = averaged_tilt_potential(positions)
-#         # p4 = averaged_roll_potential(s.positions)
-#         plt.plot(p, label=LD[i] + 'tilt')
-#         # plt.plot(p2, label=str(i) + 'roll')
-#         # plt.plot(p3, label=str(i) + 'average_tilt')
-#     plt.plot(p3, label='average_tilt')
-#     plt.legend()
-#     plt.show(block=True)
-#
-#     for i in range(4):
-#         p2 = roll_potential(positions, np.ones(len(positions), dtype=np.uint8) * i)
-#         # p2 = roll_potential(s.positions, np.ones_like(s.sequence) * i)
-#         p4 = averaged_roll_potential(positions)
-#         # p4 = averaged_roll_potential(s.positions)
-#         plt.plot(p2, label=LD[i] + 'roll')
-#         # plt.plot(p2, label=str(i) + 'roll')
-#         # plt.plot(p3, label=str(i) + 'average_tilt')
-#     plt.plot(p4, label='average_roll')
-#     plt.legend()
-#     plt.show(block=True)
-#
-#     plt.plot(p3, label='average_tilt')
-#     plt.plot(p4, label='average_roll')
-#     plt.legend()
-#     plt.show(block=True)
-#
-#     for i in range(4):
-#         p = tilt_potential(positions, np.ones(len(positions), dtype=np.uint8) * i)
-#         p2 = roll_potential(positions, np.ones(len(positions), dtype=np.uint8) * i)
-#         # p3 = averaged_tilt_potential(positions)
-#         # p4 = averaged_roll_potential(s.positions)
-#         plt.plot(p, label=LD[i] + 'tilt')
-#         plt.plot(p2, label=LD[i] + 'roll')
-#         # plt.plot(p3, label=str(i) + 'average_tilt')
-#         # plt.plot(p3, label='average_tilt')
-#         plt.legend()
-#         plt.show(block=True)
